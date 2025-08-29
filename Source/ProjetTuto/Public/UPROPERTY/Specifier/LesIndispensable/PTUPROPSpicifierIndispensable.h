0a01e7e998308987354578bc5e90b1b66270d3a9
Enguerran_COBERT <enguerran.cobert@gmail.com>
2025-08-29 11:24:59 -0400

    Add detailed UPROPERTY specifiers and reorganize access levels
    Expanded the UPROPERTY specifiers with more descriptive comments, covering `Edit`, `Visible`, and Blueprint-access settings. Reorganized properties by access levels (`Public`, `Protected`, and `Private`) for improved clarity and structure.

--- Source/ProjetTuto/Public/UPROPERTY/Specifier/LesIndispensable/PTUPROPSpicifierIndispensable.h
+++ Source/ProjetTuto/Public/UPROPERTY/Specifier/LesIndispensable/PTUPROPSpicifierIndispensable.h
@@ -22,63 +22,214 @@ class PROJETTUTO_API APTUPROPSpicifierIndispensable : public AActor
 	//--------------------------------- PUBLIC ---------------------------------//
 public:
 	//--------------------------------- CATEGORY ---------------------------------//
-	UPROPERTY(Category = Indispensables)
-	int32 MyInt_CategoryOnly = 30;
+		
+	/**
+	 * Category
+	 * 
+	 * La catégorie de cette property quand elle est affiché dans les outils blueprint. Des sous catégory peut être définie en utilisant l'opérateur '|'
+	 */
+	UPROPERTY(VisibleAnywhere, Category = "Les Indispensables|Category")
+	int32 MyInt_Category = 30;
+
+	/**
+	 * AdvancedDisplay
+	 *
+	 * Cette property va être placé dans une section "advanced (menu déroulant)", dans le panel auquel elle appartient
+	 */
+	UPROPERTY(VisibleAnywhere, Category = "Les Indispensables", AdvancedDisplay)
+	int32 MyInt_AdvancedDisplay = 30;
+
+	/**
+	 * AdvancedDisplay
+	 *
+	 * Cette property va être placé dans une section "advanced (menu déroulant)", dans le panel auquel elle appartient
+	 * Ne semble pas marché avec les sous catégories
+	 */
+	UPROPERTY(VisibleAnywhere, Category = "Indispensables|Category", AdvancedDisplay)
+	int32 MyInt_AdvancedDisplay2 = 30;
+
+	/**
+	 * AdvancedDisplay
+	 *
+	 * Cette property va être placé dans une section "advanced (menu déroulant)", dans le panel auquel elle appartient
+	 * Ne semble pas marché avec les sous catégories
+	 */
+	UPROPERTY(VisibleAnywhere, AdvancedDisplay)
+	int32 MyInt_AdvancedDisplay3 = 30;
 	
 	//--------------------------------- EDIT ---------------------------------//
+	//--------------------------------- PRIVATE ---------------------------------//
+private:
 	/**
 	 * EditAnywhere
-	 * Indicates that this property can be edited by property windows, on archetypes and instances. This Specifier is incompatible with any of the "Visible" Specifiers.
+	 * 
+	 * Indique que cette property peut être édité par les property windows, sur les archetypes et les instances. Ce specifier est incompatible avec n'importe 'Visible' specifiers.
 	 */
 	UPROPERTY(EditAnywhere, Category = "Les Indispensables")
-	int32 MyInt_EditAnywhere = 30;
+	int32 MyInt_EditAnywhere_private = 30;
 
 	/**
 	 * EditDefaultsOnly
-	 * Indicates that this property can be edited by property windows, but only on archetypes. This Specifier is incompatible with any of the "Visible" Specifiers.
+	 *
+	 * Indique que cette property peut être édité par les property windows, mais uniquement sur les archetypes. Ce specifier est incompatible avec n'importe 'Visible' specifiers.
 	 */
 	UPROPERTY(EditDefaultsOnly, Category = "Les Indispensables")
-	int32 MyInt_EditDefaultsOnly = 30;
+	int32 MyInt_EditDefaultsOnly_private = 30;
 
 	/**
 	 * EditInstanceOnly
-	 * Indicates that this property can be edited by property windows, but only on instances, not on archetypes. This Specifier is incompatible with any of the "Visible" Specifiers.
+	 * 
+	 * Indique que cette property peut être édité par les property windows, mais uniquement sur les instances. Ce specifier est incompatible avec n'importe 'Visible' specifiers.
 	 */
 	UPROPERTY(EditInstanceOnly, Category = "Les Indispensables")
-	int32 MyInt_EditInstanceOnly = 30;
+	int32 MyInt_EditInstanceOnly_private = 30;
+	
+	//--------------------------------- PROTECTED ---------------------------------//
+protected:
+	/**
+	 * EditAnywhere
+	 * 
+	 * Indique que cette property peut être édité par les property windows, sur les archetypes et les instances. Ce specifier est incompatible avec n'importe 'Visible' specifiers.
+	 */
+	UPROPERTY(EditAnywhere, Category = "Les Indispensables")
+	int32 MyInt_EditAnywhere_protected = 30;
+
+	/**
+	 * EditDefaultsOnly
+	 *
+	 * Indique que cette property peut être édité par les property windows, mais uniquement sur les archetypes. Ce specifier est incompatible avec n'importe 'Visible' specifiers.
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = "Les Indispensables")
+	int32 MyInt_EditDefaultsOnly_protected = 30;
+
+	/**
+	 * EditInstanceOnly
+	 * 
+	 * Indique que cette property peut être édité par les property windows, mais uniquement sur les instances. Ce specifier est incompatible avec n'importe 'Visible' specifiers.
+	 */
+	UPROPERTY(EditInstanceOnly, Category = "Les Indispensables")
+	int32 MyInt_EditInstanceOnly_protected = 30;
+
+	//--------------------------------- PUBLIC ---------------------------------//
+public:
+	/**
+	 * EditAnywhere
+	 * 
+	 * Indique que cette property peut être édité par les property windows, sur les archetypes et les instances. Ce specifier est incompatible avec n'importe 'Visible' specifiers.
+	 */
+	UPROPERTY(EditAnywhere, Category = "Les Indispensables")
+	int32 MyInt_EditAnywhere_public = 30;
+
+	/**
+	 * EditDefaultsOnly
+	 *
+	 * Indique que cette property peut être édité par les property windows, mais uniquement sur les archetypes. Ce specifier est incompatible avec n'importe 'Visible' specifiers.
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = "Les Indispensables")
+	int32 MyInt_EditDefaultsOnly_public = 30;
+
+	/**
+	 * EditInstanceOnly
+	 * 
+	 * Indique que cette property peut être édité par les property windows, mais uniquement sur les instances. Ce specifier est incompatible avec n'importe 'Visible' specifiers.
+	 */
+	UPROPERTY(EditInstanceOnly, Category = "Les Indispensables")
+	int32 MyInt_EditInstanceOnly_public = 30;
 	
 	//--------------------------------- VISIBLE ---------------------------------//
+	//--------------------------------- PRIVATE ---------------------------------//
+private:
 	/**
 	 * VisibleAnywhere
+	 *
+	 * Indique que cette property est visible dans toutes les property windows, mais ne peut être édité. Incompatible avec n'importe "Edit" specifiers.
 	 */
 	UPROPERTY(VisibleAnywhere, Category = "Les Indispensables")
-	int32 MyInt_VisibleAnywhere = 30;
+	int32 MyInt_VisibleAnywhere_private = 30;
 
 	/**
 	 * VisibleDefaultsOnly
+	 *
+	 * Indique que cette property est uniquement visible dans les property windows des aarchetypes, et ne peut être édité. Incompatible avec n'importe "Edit" specifiers.
 	 */
 	UPROPERTY(VisibleDefaultsOnly, Category = "Les Indispensables")
-	int32 MyInt_VisibleDefaultsOnly = 30;
+	int32 MyInt_VisibleDefaultsOnly_private = 30;
 
 	/**
 	 * VisibleInstanceOnly
+	 *
+	 * Indique que cette property est uniquement visible dans les property windows des instances, mais ne peut être édité. Incompatible avec n'importe "Edit" specifiers.
 	 */
 	UPROPERTY(VisibleInstanceOnly, Category = "Les Indispensables")
-	int32 MyInt_VisibleInstanceOnly = 30;
+	int32 MyInt_VisibleInstanceOnly_private = 30;
+
+	//--------------------------------- PROTECTED ---------------------------------//
+protected:
+	/**
+	 * VisibleAnywhere
+	 *
+	 * Indique que cette property est visible dans toutes les property windows, mais ne peut être édité. Incompatible avec n'importe "Edit" specifiers.
+	 */
+	UPROPERTY(VisibleAnywhere, Category = "Les Indispensables")
+	int32 MyInt_VisibleAnywhere_protected = 30;
+
+	/**
+	 * VisibleDefaultsOnly
+	 *
+	 * Indique que cette property est uniquement visible dans les property windows des aarchetypes, et ne peut être édité. Incompatible avec n'importe "Edit" specifiers.
+	 */
+	UPROPERTY(VisibleDefaultsOnly, Category = "Les Indispensables")
+	int32 MyInt_VisibleDefaultsOnly_protected = 30;
+
+	/**
+	 * VisibleInstanceOnly
+	 *
+	 * Indique que cette property est uniquement visible dans les property windows des instances, mais ne peut être édité. Incompatible avec n'importe "Edit" specifiers.
+	 */
+	UPROPERTY(VisibleInstanceOnly, Category = "Les Indispensables")
+	int32 MyInt_VisibleInstanceOnly_protected = 30;
+
+	//--------------------------------- PUBLIC ---------------------------------//
+public:
+	/**
+	 * VisibleAnywhere
+	 *
+	 * Indique que cette property est visible dans toutes les property windows, mais ne peut être édité. Incompatible avec n'importe "Edit" specifiers.
+	 */
+	UPROPERTY(VisibleAnywhere, Category = "Les Indispensables")
+	int32 MyInt_VisibleAnywhere_public = 30;
+
+	/**
+	 * VisibleDefaultsOnly
+	 *
+	 * Indique que cette property est uniquement visible dans les property windows des aarchetypes, et ne peut être édité. Incompatible avec n'importe "Edit" specifiers.
+	 */
+	UPROPERTY(VisibleDefaultsOnly, Category = "Les Indispensables")
+	int32 MyInt_VisibleDefaultsOnly_public = 30;
+
+	/**
+	 * VisibleInstanceOnly
+	 *
+	 * Indique que cette property est uniquement visible dans les property windows des instances, mais ne peut être édité. Incompatible avec n'importe "Edit" specifiers.
+	 */
+	UPROPERTY(VisibleInstanceOnly, Category = "Les Indispensables")
+	int32 MyInt_VisibleInstanceOnly_public = 30;
 
 	//--------------------------------- READ WRITE---------------------------------//
+	//--------------------------------- PROTECTED ---------------------------------//
+protected:
 	/**
 	 * VisibleDefaultsOnly, BlueprintReadWrite
 	 *
-	 * This property can be read or written from a Blueprint. This Specifier is incompatible with the BlueprintReadOnly Specifier.
+	 * Peut être lu et modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Les Indispensables|ReadWrite")
 	int32 MyInt_VDO_BlueprintReadWrite = 30;
 
 	/**
 	 * VisibleInstanceOnly, BlueprintReadWrite
-	 *
-	 * This property can be read or written from a Blueprint. This Specifier is incompatible with the BlueprintReadOnly Specifier.
+	 * 
+	 * Peut être lu et modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = "Les Indispensables|ReadWrite")
 	int32 MyInt_VIO_BlueprintReadWrite = 30;
@@ -86,15 +237,15 @@ public:
 	/**
 	 * VisibleAnywhere, BlueprintReadWrite
 	 *
-	 * This property can be read or written from a Blueprint. This Specifier is incompatible with the BlueprintReadOnly Specifier.
+	 * Peut être lu et modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Les Indispensables|ReadWrite")
 	int32 MyInt_VA_BlueprintReadWrite = 30;
 
 	/**
 	 * EditDefaultsOnly, BlueprintReadWrite
-	 *
-	 * This property can be read or written from a Blueprint. This Specifier is incompatible with the BlueprintReadOnly Specifier.
+	 * 
+	 * Peut être lu et modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Les Indispensables|ReadWrite")
 	int32 MyInt_EDO_BlueprintReadWrite = 30;
@@ -102,7 +253,7 @@ public:
 	/**
 	 * EditInstanceOnly, BlueprintReadWrite
 	 *
-	 * This property can be read or written from a Blueprint. This Specifier is incompatible with the BlueprintReadOnly Specifier.
+	 * Peut être lu et modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Les Indispensables|ReadWrite")
 	int32 MyInt_EIO_BlueprintReadWrite = 30;
@@ -110,16 +261,18 @@ public:
 	/**
 	 * EditAnywhere, BlueprintReadWrite
 	 *
-	 * This property can be read or written from a Blueprint. This Specifier is incompatible with the BlueprintReadOnly Specifier.
+	 * Peut être lu et modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Les Indispensables|ReadWrite")
 	int32 MyInt_EA_BlueprintReadWrite = 30;
 	
 	//--------------------------------- READ ONLY---------------------------------//
+	//--------------------------------- PROTECTED ---------------------------------//
+protected:
 	/**
 	 * VisibleDefaultsOnly, BlueprintReadOnly
-	 *
-	 * This property can be read by Blueprints, but not modified. This Specifier is incompatible with the BlueprintReadWrite Specifier.
+	 * 
+	 * Peut être lu mais ne peut pas être modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Les Indispensables|ReadOnly")
 	int32 MyInt_VDO_BlueprintReadOnly = 30;
@@ -127,7 +280,7 @@ public:
 	/**
 	 * VisibleInstanceOnly, BlueprintReadOnly
 	 *
-	 * This property can be read by Blueprints, but not modified. This Specifier is incompatible with the BlueprintReadWrite Specifier.
+	 * Peut être lu mais ne peut pas être modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Les Indispensables|ReadOnly")
 	int32 MyInt_VIO_BlueprintReadOnly = 30;
@@ -135,7 +288,7 @@ public:
 	/**
 	 * VisibleAnywhere, BlueprintReadOnly
 	 *
-	 * This property can be read by Blueprints, but not modified. This Specifier is incompatible with the BlueprintReadWrite Specifier.
+	 * Peut être lu mais ne peut pas être modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Les Indispensables|ReadOnly")
 	int32 MyInt_VA_BlueprintReadOnly = 30;
@@ -143,7 +296,7 @@ public:
 	/**
 	 * EditDefaultsOnly, BlueprintReadOnly
 	 *
-	 * This property can be read by Blueprints, but not modified. This Specifier is incompatible with the BlueprintReadWrite Specifier.
+	 * Peut être lu mais ne peut pas être modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Les Indispensables|ReadOnly")
 	int32 MyInt_EDO_BlueprintReadOnly = 30;
@@ -151,7 +304,7 @@ public:
 	/**
 	 * EditInstanceOnly, BlueprintReadOnly
 	 *
-	 * This property can be read by Blueprints, but not modified. This Specifier is incompatible with the BlueprintReadWrite Specifier.
+	 * Peut être lu mais ne peut pas être modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Les Indispensables|ReadOnly")
 	int32 MyInt_EIO_BlueprintReadOnly = 30;
@@ -159,24 +312,10 @@ public:
 	/**
 	 * EditAnywhere, BlueprintReadOnly
 	 *
-	 * This property can be read by Blueprints, but not modified. This Specifier is incompatible with the BlueprintReadWrite Specifier.
+	 * Peut être lu mais ne peut pas être modifié depuis un Blueprint. Incompatible avec le specifier BlueprintReadOnly
 	 */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Les Indispensables|ReadOnly")
 	int32 MyInt_EA_BlueprintReadOnly = 30;
-
-	//--------------------------------- CATEGORY ---------------------------------//
-	/**
-	 * Specifies the category of the property when displayed in Blueprint editing tools. Define nested categories using the | operator.
-	 */
-	UPROPERTY(VisibleAnywhere, Category = "Les Indispensables|Category")
-	int32 MyInt_Category = 30;
 	
 	//--------------------------------- ADVANCED DISPLAY ---------------------------------//
-	/**
-	 * AdvancedDisplay
-	 *
-	 * The property will be placed in the advanced (dropdown) section of any panel where it appears.
-	 */
-	UPROPERTY(VisibleAnywhere, Category = "Les Indispensables", AdvancedDisplay)
-	int32 MyInt_AdvancedDisplay = 30;
 };
