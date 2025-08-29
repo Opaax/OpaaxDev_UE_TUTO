7e3c635499da33de50f8e67973192d82d309e3d7
Enguerran_COBERT <enguerran.cobert@gmail.com>
2025-08-29 15:45:58 -0400

    [Update] Correction de faute des commentaires

--- Source/ProjetTuto/Public/UPROPERTY/Specifier/BPGetSet_GetSet/PTUPROPSpecifier_BPGetSet_GetSet.h
+++ Source/ProjetTuto/Public/UPROPERTY/Specifier/BPGetSet_GetSet/PTUPROPSpecifier_BPGetSet_GetSet.h
@@ -6,7 +6,11 @@
 #include "GameFramework/Actor.h"
 #include "PTUPROPSpecifier_BPGetSet_GetSet.generated.h"
 
-/***/
+/**
+ * @class APTUPROPSpecifier_BPGetSet_GetSet
+ *
+ * Example et doc pour les specifiers BlueprintGetter - BlueprintSetter
+ */
 UCLASS()
 class PROJETTUTO_API APTUPROPSpecifier_BPGetSet_GetSet : public AActor
 {
@@ -16,27 +20,27 @@ class PROJETTUTO_API APTUPROPSpecifier_BPGetSet_GetSet : public AActor
 	//--------------------------------- PUBLIC ---------------------------------//
 public:
 	/***/
-	UFUNCTION(BlueprintPure, Category = "BPGetSet")
+	UFUNCTION(BlueprintGetter, Category = "BPGetSet")
 	FString GetMyString_BPGetter() const;
 
 	/***/
-	UFUNCTION(BlueprintCallable, Category = "BPGetSet")
+	UFUNCTION(BlueprintSetter, Category = "BPGetSet")
 	void SetMyString_BPSetter(const FString& NewString );
 
 	/***/
-	UFUNCTION(BlueprintPure, Category = "BPGetSet")
+	UFUNCTION(BlueprintGetter, Category = "BPGetSet")
 	FString GetMyString_BPGetterSetter() const;
 
 	/***/
-	UFUNCTION(BlueprintCallable, Category = "BPGetSet")
+	UFUNCTION(BlueprintSetter, Category = "BPGetSet")
 	void SetMyString_BPGetterSetter(const FString& NewString );
 
 	/***/
-	UFUNCTION(BlueprintPure, Category = "BPGetSet")
+	UFUNCTION(BlueprintGetter, Category = "BPGetSet")
 	FString GetMyString_ReadWrite_GetSet() const;
 
 	/***/
-	UFUNCTION(BlueprintCallable, Category = "BPGetSet")
+	UFUNCTION(BlueprintSetter, Category = "BPGetSet")
 	void SetMyString_ReadWrite_GetSet(const FString& NewString);
 
 	/**
@@ -57,14 +61,14 @@ public:
 	//--------------------------------- PROTECTED ---------------------------------//
 protected:
 	/**
-	 * En arrière plan, UHT créer des 'Getter - Setter' pour les properties avec le specifier BlueprintReadWrite
+	 * En arrière-plan, UHT crée des 'Getter - Setter' pour les property avec le specifier BlueprintReadWrite.
 	 */
 	UPROPERTY(BlueprintReadWrite)
 	FString MyString_ReadWrite = TEXT("MyString_ReadWrite");
 
 	/**
-	 * En revanche on peut customiser quel function on veut pour le specifier BlueprintReadWrite
-	 * Ça nous donne l'avantage de faire plus d'opération que 'seulement' get ou set la valeur d'un membre
+	 * En revanche, on peut personnaliser quelles fonctions on veut utiliser pour le specifier BlueprintReadWrite.
+	 * Cela nous donne l’avantage de pouvoir effectuer plus d’opérations que 'seulement' obtenir ou modifier la valeur d’un membre.
 	 */
 	UPROPERTY(BlueprintReadWrite, BlueprintGetter = "GetMyString_ReadWrite_GetSet", BlueprintSetter = "SetMyString_ReadWrite_GetSet")
 	FString MyString_ReadWrite_GetSet = TEXT("MyString_ReadWrite_GetSet");
@@ -72,37 +76,37 @@ protected:
 	//--------------------------------- PRIVATE ---------------------------------//
 private:
 	/**
-	 * BlueprintGetter:
-	 * Doit avoir une valeur sous forme de string
-	 * Doit être une fonction UFUNCTION()
+	 * BlueprintGetter :
+	 * - Doit avoir une valeur sous forme de string.
+	 * - Doit être une fonction UFUNCTION() pure, le specifier UFUNCTION(BlueprintGetter) gère ça automatiquement.
 	 *
-	 * Nous permet de customiser le fonction getter qu'on veut.
-	 * Est implécitement BlueprintReadOnly si aucun des BlueprintSetter, BlueprintReadWrite est spécifier
+	 * Cela nous permet de personnaliser la fonction getter que l’on souhaite.
+	 * Est implicitement BlueprintReadOnly si aucun BlueprintSetter ou BlueprintReadWrite n’est spécifié.
 	 */
 	UPROPERTY(BlueprintGetter = "GetMyString_BPGetter")
 	FString MyString_BPGetter = TEXT("MyString_BPGetter");
 
 	/**
-	 * BlueprintSetter:
-	 * Doit avoir une valeur sous forme de string
-	 * Doit être une fonction UFUNCTION()
+	 * BlueprintSetter :
+	 * - Doit avoir une valeur sous forme de string.
+	 * - Doit être une fonction UFUNCTION(), Le Specifier UFUNCTION(BlueprintSetter) gère ça automatiquement.
 	 * 
-	 * Nous permet de customiser le fonction setter qu'on veut.
-	 * Est implécitement tag comme BlueprintReadWrite (est donc un getter par défault est créé)
+	 * Cela nous permet de personnaliser la fonction setter que l’on souhaite.
+	 * Est implicitement marqué comme BlueprintReadWrite (un getter par défaut est donc créé).
 	 */
 	UPROPERTY(BlueprintSetter = "SetMyString_BPSetter")
 	FString MyString_BPSetter = TEXT("MyString_BPSetter");
 
 	/**
-	 * les deux en même temps
+	 * Les deux en même temps.
 	 */
 	UPROPERTY(BlueprintGetter = "GetMyString_BPGetterSetter", BlueprintSetter = "SetMyString_BPGetterSetter")
 	FString MyString_BPGetterSetter = TEXT("MyString_BPGetterSetter");
 
 	/**
-	 * BlueprintGetter:
-	 * Doit avoir une valeur sous forme de string.
-	 * En revanche, si valeur (la string) est vide un getteur default est créé.
+	 * BlueprintGetter :
+	 * - Doit avoir une valeur sous forme de string.
+	 * - En revanche, si la valeur (la string) est vide, un getter par défaut est créé.
 	 */
 	UPROPERTY(BlueprintGetter = "")
 	FString MyString_BPGet_NoName = TEXT("MyString_BPGet_NoName");
@@ -116,14 +120,15 @@ private:
 	//--------------------------------- PROTECTED ---------------------------------//
 protected:
 	/**
-	 * Getter et Setter donc 2 specifier assez obscure.
-	 * Pour l'instant j'ai pas trouvé quel module les ajoutes. (pas de trace dans la doc, ni dans les classes on sont les autres specifier)
+	 * Getter et Setter sont donc deux specifier assez obscurs.
+	 * Pour l’instant, je n’ai pas trouvé quel module les ajoute (pas de trace dans la doc, ni dans les classes où sont les autres specifier).
 	 *
-	 * Elle marche globalement comme BlueprintGetter/BlueprintSetter MAIS:
-	 * - Il y a pas de tag implécite (BlueprintReadWrite, BlueprintReadOnly)
-	 * - N'a pas besoin de valeur (de nom de function)
-	 * - N'a pas besoin d'être une UFUNCTION (elles peuvent l'être)
+	 * Ils fonctionnent globalement comme BlueprintGetter/BlueprintSetter MAIS :
+	 * - Il n’y a pas de tag implicite (BlueprintReadWrite, BlueprintReadOnly).
+	 * - Ils n’ont pas besoin de valeur (nom de fonction).
+	 * - Ils n’ont pas besoin d’être des UFUNCTION (mais ils peuvent l’être).
 	 */
 	UPROPERTY(BlueprintReadWrite, Getter, Setter)
 	FString MyString_GetSet = TEXT("MyString_GetSet");
+
 };
